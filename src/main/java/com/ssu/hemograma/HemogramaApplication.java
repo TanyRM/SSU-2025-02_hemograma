e8e4873179275afa47845c4f252f642e419d00ea
Taniele Rocha Madureira <taniele.madureira@seduc.go.gov.br>
2025-09-03 14:30:52 -0300

    Inicializar projeto com FHIR

---
+++ src/main/java/com/ssu/hemograma/HemogramaApplication.java
@@ -0,0 +1,13 @@
+package com.ssu.hemograma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HemogramaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HemogramaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssu/hemograma/HemogramaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssu.hemograma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HemogramaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
