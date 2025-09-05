bd7d978c1ea620ff0dfdc83c95b939f782c741c5
TanyRM <tanielemadu@outlook.com>
2025-09-05 16:47:26 -0300

    Criar base para receptor de mensagens FHIR

---
+++ src/main/java/com/ssu/hemograma/HemogramaApplication.java
@@ -0,0 +1,15 @@
+package com.ssu.hemograma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+// ignorar banco de dados por enquanto, quando necessário retirar o exclude da anotação
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class HemogramaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HemogramaApplication.class, args);
+    }
+
+}
